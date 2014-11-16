3329b6f50350ce730827c2fc44a5d03f25c222b5
Tommy Lin <tommylin1998@outlook.com>
2014-11-15 20:05:30 -0500

    Basic Program Finished

--- coinGuessingGame.py
+++ coinGuessingGame.py
@@ -1,1 +1,37 @@
 #! /usr/bin/env python3
+
+import random
+
+f = open('highScore.txt', 'r+')
+
+highScore = int(f.read())
+
+print('Coin Guessing Game.   All time high score: {}\n'.format(highScore))
+
+score = 0
+
+while True:
+    sides = ['heads', 'tails']
+    answer = str(random.choice(sides))
+    response = input('Predict heads or tails. ')
+    if response.lower() == answer:
+        score += 1
+        print('It is {}.   Your score is: {}'.format(answer, score))
+    else:
+        break
+print('It is {}.   Game over.'.format(answer))
+
+if score > highScore:
+    f.seek(0)
+    highScore = score
+    f.write(str(highScore))
+print('Your Score: {}   High Score: {}'.format(score, highScore))
+
+play = input('Do you want to start over? [Y] or [N] ')
+
+if play.lower() == 'y':
+    f.seek(0)
+    f.write('0')
+
+elif play.lower() == 'n':
+    exit()
