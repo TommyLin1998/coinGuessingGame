b70021097ef7dbc322e31f3580f39d75b7219c82
Tommy Lin <tommylin1998@outlook.com>
2014-11-22 20:22:53 -0500

    Bug fixed and improved

--- coinGuessingGame.py
+++ coinGuessingGame.py
@@ -11,27 +11,27 @@ print('Coin Guessing Game.   All time high score: {}\n'.format(highScore))
 score = 0
 
 while True:
-    sides = ['heads', 'tails']
+    sides = ['h', 't']
     answer = str(random.choice(sides))
     response = input('Predict heads or tails. ')
-    if response.lower() == answer:
+    if response[0].lower() == answer:
         score += 1
         print('It is {}.   Your score is: {}'.format(answer, score))
     else:
-        break
-print('It is {}.   Game over.'.format(answer))
+        print('It is {}.   Game over.'.format(answer))
 
+        play = input('Do you want to start over? [Y] or [N] ')
+
+        if play.lower() == 'y':
+            f.seek(0)
+            f.write('0')
+            score = 0
+            continue
+        elif play.lower() == 'n':
+            break
+        
 if score > highScore:
     f.seek(0)
     highScore = score
     f.write(str(highScore))
 print('Your Score: {}   High Score: {}'.format(score, highScore))
-
-play = input('Do you want to start over? [Y] or [N] ')
-
-if play.lower() == 'y':
-    f.seek(0)
-    f.write('0')
-
-elif play.lower() == 'n':
-    exit()
